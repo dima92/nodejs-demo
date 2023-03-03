14d17f9a6015ef3dfaef988c93f1a142dfd06dfa
dima92 <dima-tkachenko@mail.ru>
2023-03-03 12:22:37 +0300

    Router

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import { userRouter } from './users/users.js';
 
 const port = 8000
 const app = express()
@@ -6,6 +7,8 @@ const app = express()
 app.get('/hello', (req, res) => {
   res.end();
 });
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
   console.log(`Сервер запущен на http://localhost:${port}`)
