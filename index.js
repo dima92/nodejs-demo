e9c082b85cd868507acbb475a02829c6466b8c4f
dima92 <dima-tkachenko@mail.ru>
2023-03-03 12:38:30 +0300

    Начало работы с TypeScript

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-import express from 'express'
-import { userRouter } from './users/users.js';
-
-const port = 8000
-const app = express()
-
-app.use((req, res, next) => {
-  console.log('Время ', Date.now());
-  next();
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var users_js_1 = require("./users/users.js");
+var port = 8000;
+var app = (0, express_1["default"])();
+app.use(function (req, res, next) {
+    console.log('Время ', Date.now());
+    next();
 });
-
-app.get('/hello', (req, res) => {
-  throw new Error('Error!!!');
+app.get('/hello', function (req, res) {
+    throw new Error('Error!!!');
 });
-
-app.use('/users', userRouter);
-
-app.use((err, req, res, next) => {
-  console.log(err.message);
-  res.status(401).send(err.message);
+app.use('/users', users_js_1.userRouter);
+app.use(function (err, req, res, next) {
+    console.log(err.message);
+    res.status(401).send(err.message);
 });
-
-
-app.listen(port, () => {
-  console.log(`Сервер запущен на http://localhost:${port}`)
-})
+app.listen(port, function () {
+    console.log("\u0421\u0435\u0440\u0432\u0435\u0440 \u0437\u0430\u043F\u0443\u0449\u0435\u043D \u043D\u0430 http://localhost:".concat(port));
+});
--- users/users.js
+++ users/users.js
@@ -1,18 +1,16 @@
-import express from 'express';
-
-const userRouter = express.Router();
-
-userRouter.use((req, res, next) => {
-  console.log('Обработчик users');
-  next();
+"use strict";
+exports.__esModule = true;
+exports.userRouter = void 0;
+var express_1 = require("express");
+var userRouter = express_1["default"].Router();
+exports.userRouter = userRouter;
+userRouter.use(function (req, res, next) {
+    console.log('Обработчик users');
+    next();
 });
-
-userRouter.post('/login', (req, res) => {
-  res.send('login');
+userRouter.post('/login', function (req, res) {
+    res.send('login');
 });
-
-userRouter.post('/register', (req, res) => {
-  res.send('register');
+userRouter.post('/register', function (req, res) {
+    res.send('register');
 });
-
-export {userRouter}
